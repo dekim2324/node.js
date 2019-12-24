8092e12b46f852ab1b16f30ee13f6a3851977892
Dennis Kim <dekim2324@gmail.com>
2019-12-24 12:10:39 -0600

    fs file system

---
+++ fs.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+// fs.writeFile('message.txt', 'Hello there node', err => {
+//     if(err) throw err;
+    
+//     console.log('file has been written')
+// })
+
+
+
+fs.readFile('message.txt', 'utf8', (err, data) => {
+    if(err) throw err;
+
+    console.log(`successful! Here is the data: ${data}`)
+})
