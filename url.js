de9af6b61fe184eb61fdc3dd0e2d3b35feba7d7a
Dennis Kim <dekim2324@gmail.com>
2019-12-24 11:59:06 -0600

    url files

--- url.js
+++ url.js
@@ -1,1 +1,9 @@
 const url = require('url')
+
+const address = 'https://github.com/dekim2324?tab=repositories';
+
+const parsedURL = url.parse(address, true)
+
+console.log(parsedURL.host);
+console.log(parsedURL.pathname);
+console.log(parsedURL.query);
